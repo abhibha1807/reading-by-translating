eebd749867e579217be71df9a54e9d1b91ac7508
Abhibha Gupta <abhibha1807@gmail.com>
2021-07-14 01:07:29 +0530

    activate base

--- train.sh
+++ train.sh
@@ -3,7 +3,7 @@ nvidia-smi
 # conda env create -f environment.yml
 # conda update -n yxy -c defaults conda
 # conda install -n yxy -c huggingface transformers
-source activate yxy
+source activate base
 # conda install -c huggingface transformers
 # conda install -c conda-forge tensorboard
 # conda install -c pytorch torchtext
