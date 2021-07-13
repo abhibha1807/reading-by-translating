ce1621fce398bfe7721854a06d2ef418840d8a99
Abhibha Gupta <abhibha1807@gmail.com>
2021-07-14 01:05:04 +0530

    activate environ

--- train.sh
+++ train.sh
@@ -3,7 +3,7 @@ nvidia-smi
 # conda env create -f environment.yml
 # conda update -n yxy -c defaults conda
 # conda install -n yxy -c huggingface transformers
-# source activate yxy
+source activate yxy
 # conda install -c huggingface transformers
 # conda install -c conda-forge tensorboard
 # conda install -c pytorch torchtext
