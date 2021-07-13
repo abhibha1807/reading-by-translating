441d8587459c62b8ba1e080029f949c3a8ba14a3
Abhibha Gupta <abhibha1807@gmail.com>
2021-07-13 23:05:36 +0530

    solve environ

--- train.sh
+++ train.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 nvidia-smi
-# conda env create -f environment.yml
-# conda update -n base -c defaults conda
-# source activate yxy
+conda env create -f environment.yml
+conda update -n base -c defaults conda
+source activate yxy
 # conda install -c huggingface transformers
 # conda install -c conda-forge tensorboard
-conda install -c pytorch torchtext
+# conda install -c pytorch torchtext
 python main_.py
