5d5c1e237e8eddc7fa8e83b4f961874e8e063e66
Abhibha Gupta <abhibha1807@gmail.com>
2021-07-14 01:09:33 +0530

    install tensorboard base

--- train.sh
+++ train.sh
@@ -2,7 +2,7 @@
 nvidia-smi
 # conda env create -f environment.yml
 # conda update -n yxy -c defaults conda
-# conda install -n yxy -c huggingface transformers
+conda install -n base -c conda-forge tensorboard
 source activate base
 # conda install -c huggingface transformers
 # conda install -c conda-forge tensorboard
