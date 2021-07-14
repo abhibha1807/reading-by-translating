ed9eb033322af8631a716fdc82fcadd798f52c35
Abhibha Gupta <abhibha1807@gmail.com>
2021-07-14 11:38:46 +0530

    ...

--- train.sh
+++ train.sh
@@ -5,7 +5,7 @@ nvidia-smi
 # conda install -n base -c conda-forge tensorboard
 # conda install -n base -c conda-forge huggingface transformers
 # source activate rbt
-# conda init bash
+conda init bash
 conda activate rbt
 # conda install -c huggingface transformers
 # conda install -c conda-forge tensorboard
