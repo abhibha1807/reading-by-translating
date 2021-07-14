eb13474b939a33282af44aff1d459bd6a7dcbfa7
Abhibha Gupta <abhibha1807@gmail.com>
2021-07-15 00:22:04 +0530

    ...

--- train.sh
+++ train.sh
@@ -14,7 +14,7 @@ nvidia-smi
 # set -e 
 # source //abhibha-volume/rbt/bin/activate
 eval "$(conda shell.bash hook)"
-conda activate ../../opt/conda/envs/rbt-conda
+conda activate rbt-conda
 echo $VIRTUAL_ENV
 which python
 pip list --local
