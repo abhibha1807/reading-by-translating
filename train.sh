2289fa13bf1eb5326981dc93c03933b25950002e
Abhibha Gupta <abhibha1807@gmail.com>
2021-07-16 17:06:47 +0530

    pip3-> pip

--- train.sh
+++ train.sh
@@ -31,6 +31,6 @@ pwd
 # cd abhibha-volume
 source rbt/bin/activate
 which python
-/abhibha-volume/rbt/lib/python3.6/site-packages/pip3 list --local
+/abhibha-volume/rbt/lib/python3.6/site-packages/pip list --local
 # cd reading-by-translating
 /abhibha-volume/rbt/bin/python /abhibha-volume/reading-by-translating/main_.py
