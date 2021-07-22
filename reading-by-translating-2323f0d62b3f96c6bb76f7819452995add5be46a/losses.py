0379a4ee13595e9c6dbac90576411473ec98201c
Abhibha Gupta <abhibha1807@gmail.com>
2021-07-22 22:28:07 +0530

    removed debug logs and fixed shape in loss

--- reading-by-translating-2323f0d62b3f96c6bb76f7819452995add5be46a/losses.py
+++ reading-by-translating-2323f0d62b3f96c6bb76f7819452995add5be46a/losses.py
@@ -9,7 +9,7 @@ def compute_loss1(predictions, targets, a, device, criterion):
 
     predictions = predictions[:, :-1, :].contiguous()
     targets = targets[:, 1:]
-    batch_loss=torch.empty(a.shape[0], device=device)
+    batch_loss=torch.empty(targets.shape[0], device=device)
     for i in range(a.shape[0]):
       batch_loss[i]=criterion(predictions[i], targets[i])
     y=torch.zeros(1, device=device)
