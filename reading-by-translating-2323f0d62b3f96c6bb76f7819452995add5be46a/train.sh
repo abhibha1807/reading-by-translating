71aedc1c3b3e56be7d964a695c48e6741e872b55
Abhibha Gupta <abhibha1807@gmail.com>
2021-07-22 10:27:55 +0530

    add train.sh

--- reading-by-translating-2323f0d62b3f96c6bb76f7819452995add5be46a/train.sh
+++ reading-by-translating-2323f0d62b3f96c6bb76f7819452995add5be46a/train.sh
@@ -41,4 +41,4 @@ pwd
 source activate /abhibha-volume/envs
 conda info
 which python
-python /abhibha-volume/reading-by-translating-2323f0d62b3f96c6bb76f7819452995add5be46a/main_.py
+python /abhibha-volume/reading-by-translating/reading-by-translating-2323f0d62b3f96c6bb76f7819452995add5be46a/main_.py
