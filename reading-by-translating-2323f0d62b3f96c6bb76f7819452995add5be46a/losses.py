02c9ea18f8061e5a87f5f3d27565f24aaa98dbd6
Abhibha Gupta <abhibha1807@gmail.com>
2021-07-26 11:58:39 +0530

    a to cuda

--- reading-by-translating-2323f0d62b3f96c6bb76f7819452995add5be46a/losses.py
+++ reading-by-translating-2323f0d62b3f96c6bb76f7819452995add5be46a/losses.py
@@ -1,4 +1,5 @@
 import torch
+from torch import cuda
 '''
 Implements loss functions for step 1,2 and 3 of the pipeline.
 compute_loss1-> calculates batch loss for step1 by multiplying A
@@ -6,7 +7,7 @@ compute_loss2-> calculates batch loss for step 2 and 3
 '''
 #computes loss for step 1
 def compute_loss1(predictions, targets, a, device, criterion):
-
+    a=a.to(cuda)
     predictions = predictions[:, :-1, :].contiguous()
     targets = targets[:, 1:]
     batch_loss=torch.empty(targets.shape[0], device=device)
