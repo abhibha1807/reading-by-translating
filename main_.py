881da7571f4cb2124a552159d0476dfc70985427
Abhibha Gupta <abhibha1807@gmail.com>
2021-08-23 00:19:45 +0530

    step 1 + 2

--- main_.py
+++ main_.py
@@ -152,8 +152,8 @@ def run():
         epoch_loss1 = mdl.train_model1(A_batch, train_dataloader, optimizer1, de_tokenizer, criterion, )
         writer.add_scalar('Loss/model1', epoch_loss1, epoch)
       
-        # epoch_loss2 = mdl.train_model2(unlabeled_dataloader, optimizer2, de_tokenizer, criterion, )# using the same training dataset for now.
-        # writer.add_scalar('Loss/model2', epoch_loss2, epoch)
+        epoch_loss2 = mdl.train_model2(unlabeled_dataloader, optimizer2, de_tokenizer, criterion, )# using the same training dataset for now.
+        writer.add_scalar('Loss/model2', epoch_loss2, epoch)
        
         # # epoch_loss3, a_ind = mdl.val_model2( valid_dataloader, optimizer3, A, A_batch , de_tokenizer, criterion, scheduler3, a_ind)
         # #scheduler3
