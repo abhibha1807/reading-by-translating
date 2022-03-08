e9726521a2ffee8af9becf7435627257b46262b0
Abhibha Gupta <abhibha1807@gmail.com>
2022-03-08 15:51:12 +0530

    removed old files

--- train.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-nvidia-smi
-pwd
-source activate /abhibha-volume/envs
-conda info
-which python
-# python Model.py
-python /abhibha-volume/reading-by-translating/main_.py
-# python trainTokenizer.py
