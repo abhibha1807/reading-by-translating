5712939200e0fc34cfc91d481566ddb240a1eef7
Abhibha Gupta <abhibha1807@gmail.com>
2022-03-09 13:29:45 +0530

    remove activate environ

--- train.sh
+++ train.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 nvidia-smi
 pwd
-source activate /abhibha-volume/envs
-conda info
+# source activate /abhibha-volume/envs
+# conda info
 which python
-# python Model.py
 python /abhibha-volume/reading-by-translating/main.py
-# python trainTokenizer.py
