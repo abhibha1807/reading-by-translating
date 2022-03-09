4db88afd1074209e9b85cb043cbbe8616668b92b
Abhibha Gupta <abhibha1807@gmail.com>
2022-03-09 14:51:30 +0530

    activate ejnviron

--- train.sh
+++ train.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 nvidia-smi
 pwd
-# source activate /abhibha-volume/envs
-# conda info
+source activate /abhibha-volume/envs
+conda info
 which python
+# pip install utils
+# pip install 
 python /abhibha-volume/reading-by-translating/main.py
