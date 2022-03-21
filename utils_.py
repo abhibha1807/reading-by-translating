53ba5a85e0b860f98b8ff0c40e7b3d5739ae34b7
Abhibha Gupta <abhibha1807@gmail.com>
2022-03-22 00:16:07 +0530

    beamwidth 5

--- utils_.py
+++ utils_.py
@@ -111,7 +111,7 @@ def beam_decode(target_tensor, decoder_hiddens, decoder, encoder_outputs=None):
     :param encoder_outputs: if you are using attention mechanism you can pass encoder outputs, [T, B, H] where T is the maximum length of input sentence
     :return: decoded_batch
     '''
-    beam_width = 10
+    beam_width = 5
     topk = 1  # how many sentence do you want to generate
     decoded_batch = []
 
