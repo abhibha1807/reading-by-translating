5ac71b371a7f48d9b43322d9afc9683b5b2e2640
Abhibha Gupta <abhibha1807@gmail.com>
2022-03-25 18:47:05 +0530

    commented relu

--- Enc_Dec.py
+++ Enc_Dec.py
@@ -90,7 +90,7 @@ class AttnDecoderRNN(nn.Module):
         output = torch.cat((embedded[0], attn_applied[0]), 1)
         output = self.attn_combine(output).unsqueeze(0)
 
-        output = F.relu(output)
+        #output = F.relu(output)
         output, hidden = self.gru(output, hidden)
 
         output = F.log_softmax(self.out(output[0]), dim=1)
