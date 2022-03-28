fdde550d59d9d364861800845ab734aff4dd38ab
Abhibha Gupta <abhibha1807@gmail.com>
2022-03-28 15:58:22 +0530

    grad clip < 1 = 0.5

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ parser.add_argument('--epochs', type=int, default=300, help='num of training epo
 
 parser.add_argument('--batch_size', type=int, default=1, help='batch size')
 ####################################################################################
-parser.add_argument('--grad_clip', type=float, default=5, help='gradient clipping')
+parser.add_argument('--grad_clip', type=float, default=0.5, help='gradient clipping')
 parser.add_argument('--A_lr', type=float, default=3e-4, help='learning rate for A')
 #reduce lr 
 # parser.add_argument('--A_lr', type=float, default=1e-6, help='learning rate for A')
