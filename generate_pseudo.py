7c4797e5103b626d4d90ee3954ebbfe41668c5be
Abhibha Gupta <abhibha1807@gmail.com>
2022-05-31 01:55:38 +0530

    try print sent

--- generate_pseudo.py
+++ generate_pseudo.py
@@ -137,6 +137,8 @@ logging.info("args = %s", args)
 # print(random.choice(pairs))
 
 train_iter, valid_iter, test_iter = IWSLT2017()
+src_sentence, tgt_sentence = next(iter(train_iter))
+print(src_sentence, tgt_sentence)
 
 pairs = []
 train_pairs = []
